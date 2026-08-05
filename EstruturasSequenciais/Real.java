daddf8a068492e7460bd22c974bdcbcc506202c0
Isadora <isaguedescavalcante@gmail.com>
2026-08-05 14:13:43 +0000

    Exercícios novos e corrigidos

---
+++ EstruturasSequenciais/Real.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Real {
+     public static void main(String[] args){
+    Scanner ler = new Scanner(System.in);
+
+    double real;
+
+    System.out.println("Digite um numero real: ");
+    real = ler.nextDouble();
+
+    System.out.println("Numero: "+ real);
+     }
+}
---
+++ EstruturasSequenciais/nome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class nome {
+    public static void main(String[] args){
+       Scanner ler = new Scanner (System.in);
+       String nome;
+         System.out.println("Digite seu nome: ");
+         nome = ler.nextLine();
+
+         System.out.printf("%s\n", nome);
+
+    }
+}
